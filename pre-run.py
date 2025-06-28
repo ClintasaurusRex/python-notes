d8a207315a9a98475e97938234749149eb7ba972
Clint <clinthiles1992@gmail.com>
2025-06-28 16:09:39 -0700

    refactor: Remove unused code and clean up pre-run.py

--- pre-run.py
+++ pre-run.py
@@ -1,44 +1,4 @@
-
-
-
-
-# def not_mutual_friends(list1, list2):
-#     # Write your code below
-#     only_lst1 = [friend for friend in list1 if friend not in list2]
-#     only_lst2 = [friend for friend in list2 if friend not in list1]
-   
-
-#     non_mutual = only_lst1 + only_lst2
-#     return(non_mutual)
-  
-
-# not_mutual_friends(["John", "Emma", "Mike", "Sarah"], ["Emma", "Tom", "Sarah", "Peter"])
-
-
-
-
-
-
-# lst1 = input().split(",")
-# lst2 = input().split(",")
-# # Write your code below
-# new_lst = []
-# for item in lst1:
-#   if item not in lst2:
-#     new_lst.append(item)
-# print(new_lst)
-
-
-
-# numbers = input().split()
-# # Write your code below
-# reverse_lst = numbers[::-1]
-# first = numbers[:1]
-# last = numbers[-1:]
-# new_lst = first + numbers + reverse_lst + last
-# repeat = new_lst * 2
-# print(repeat)
-
-
-
-
+n = int(input())
+rows = int(n/2)+1
+for i in range(rows):
+  print("*" * (2 * i + 1))
