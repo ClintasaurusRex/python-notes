eb39d0bac904745869f0b35d6b8ac09ead0527ad
Clint <clinthiles1992@gmail.com>
2025-08-04 09:16:06 -0700

    Adds basic list comprehension examples and challenges
    Introduces several list comprehension examples demonstrating basic operations such as doubling numbers, calculating word lengths, filtering and squaring positive numbers, and summing positive even numbers.
    
    These additions serve as practice challenges to enhance understanding of list comprehensions in Python.

--- pre-run.py
+++ pre-run.py
@@ -1,5 +1,55 @@
 
+# -------------------------------------- Basic List Comprehensions ---------------------------------
+print("-------------------------------------Challenge One ------------------------------------------")
+nums = [1,2,3,4,5]
+def double_numbers(numbers):
+    return [n* 2 for n in numbers]
 
+print(double_numbers(nums))
+
+print("------------------------------------Challenge Two --------------------------------------------")
+# -------------------------------------- Creating Simple Lists ---------------------------------------
+l1 = ["python", "list", "comprehension", "challenge"]
+
+def get_word_length(words):
+    word_length = [len(word) for word in words]
+    return word_length
+
+print(get_word_length(l1))
+
+# length = [len(item) for item in l1]
+# print(length)
+
+print("------------------------------------Challenge Three --------------------------------------------")
+#  ---------------------------------------- Adding Conditions ------------------------------------------
+l2 = [-3, -2, 0, 1, 2, 3]
+
+def filter_and_square(numbers):
+    new_list = [n * n for n in numbers if n > 0]
+    return new_list
+
+print(filter_and_square(l2))
+# new_list = [n * n for n in l2 if n > 0]
+# print(new_list)
+
+print("------------------------------------Challenge Four --------------------------------------------")
+#  ---------------------------------------- Using Data Aggregation ------------------------------------------
+numbers = [-10, -5, 0, 2, 4, 7, 10, 12]
+
+def sum_positive_numbers(numbers):
+    posi_nums = sum([n for n in numbers if n > 0 and n % 2 ==0])
+    return posi_nums
+
+print(sum_positive_numbers(numbers))
+
+# sum_positive = sum([n for n in numbers if n > 0 and n % 2 == 0])
+# print(sum_positive)
+
+print("------------------------------------Challenge Five --------------------------------------------")
+#  ---------------------------------------- House of Lists ------------------------------------------
+
+
+# -------------------------------------- Basic List Comprehensions ---------------------------------
 
 # # Starter code: Multi-level dictionary practice
 
